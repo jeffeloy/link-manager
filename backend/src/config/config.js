7a897ec3329c228a4e9a3de34e23a61de7f89981
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-22 11:20:59 -0300

    :bug: Fixed bug in configuration of Sequelize

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,27 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    operatorsAliases: "0",
-  },
-  test: {
     username: "root",
-    password: null,
-    database: "database_test",
+    password: "",
+    database: "link_manager",
     host: "127.0.0.1",
     dialect: "mysql",
-    operatorsAliases: "0",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: "0",
+    socketPath: "C:/xampp/mysql/mysql.sock",
   },
 };
