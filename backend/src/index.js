4d22f6d4afa962d1e4e90d8674980c0826ab2bcb
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-21 22:58:09 -0300

    :wrench: Configured Sequelize ORM

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const db = require("./models");
+
 const routes = require("./routes");
 
 const app = express();
 
 app.use(routes);
 
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Sucess!");
+  })
+  .catch(() => {});
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333");
 });
