8f52520528fe7be109cbc9f164d89c35caac5019
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-30 11:05:03 -0300

    :rocket: Added login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+import logoImg from "../../assets/logo.png";
+import linkImg from "../../assets/link.svg";
+
+import "./styles.css";
+
 const Login = () => {
-  return <h1>Login</h1>
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImg} alt="Link Manager" className="logo" />
+
+        <form>
+          <h1>Faça seu login</h1>
+
+
+          <input
+            type="email"
+            className="input"
+            placeholder="E-mail"
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+          />
+
+          <button type="submit" className="button">Entrar</button>
+
+          <a className="back-link" href="/register">
+            {/* <FiLogIn size={16} color="#E02041"/> */}
+            Cadastre-se
+            </a>
+        </form>
+      </section>
+
+      <img src={linkImg} alt="Link"/>
+    </div>
+  );
 };
 
 export default Login;
