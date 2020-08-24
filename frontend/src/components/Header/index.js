e15e9a04de2ca5012411396cb318de3755f62ad0
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-08-24 12:29:40 -0300

    :art: Added new properties in header component

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,26 +1,31 @@
 import React from "react";
-import logoImg from "../../assets/logo.png";
-import { FiPower } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
-const Header = () => {
+const Header = ({ title, children }) => {
   return (
-    <header>
-      <nav className="nav-bar">
-        <ul className="nav-links">
-          <li>
-            <a href="#">Voltar</a>
-          </li>
-          <li>
-            <a href="#">Links</a>
-          </li>
-          <li>
-            <a href="#">Sair</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav className="nav-bar">
+          <ul className="nav-links">
+            <li>
+              <a href="#">Voltar</a>
+            </li>
+            <li>
+              <Link to="/links">Links</Link>
+            </li>
+            <li>
+              <a href="#">Sair</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="header">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </>
   );
 };
 
