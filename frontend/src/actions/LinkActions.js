5cb740d34082cd5dcc839dd7d959364125e62eea
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-09-10 12:01:34 -0300

    :sparkles: Implemented listing links with utilizing api

--- frontend/src/actions/LinkActions.js
+++ frontend/src/actions/LinkActions.js
@@ -1,10 +1,16 @@
-import { apiPost } from "../helpers/api";
+import { apiGet, apiPost } from "../helpers/api";
 
 export const LINK_CREATE = "LINK_CREATE";
+export const LINK_LIST = "LINK_LIST";
 
 export const linkCreate = (data) => {
   const isSocial = !!data.isSocial;
 
   const payload = apiPost("/links", { ...data, isSocial });
   return { type: LINK_CREATE, payload };
+};
+
+export const linkList = () => {
+  const payload = apiGet("/links");
+  return { type: LINK_LIST, payload };
 };
--- frontend/src/pages/ListLinks/index.js
+++ frontend/src/pages/ListLinks/index.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { FiPlusSquare, FiEdit, FiTrash2 } from "react-icons/fi";
 
 import Header from "../../components/Header";
+import { linkList } from "../../actions/LinkActions";
 
+import imageDefault from "../../assets/image.jpg";
 import "./styles.css";
 
-const ListLinks = () => {
+const ListLinks = ({ links, linkList }) => {
+  useEffect(() => {
+    linkList();
+  }, [linkList]);
+
   return (
     <div className="container">
       <Header
@@ -18,27 +25,42 @@ const ListLinks = () => {
           </Link>
         }
       ></Header>
-      <div className="content">
-        {/* <div className="link-img">
-          <img src="" alt="" />
-        </div>
+      {links && links.length
+        ? links.map((link) => {
+            return (
+              <div className="content-links">
+                <div className="link-content">
+                  <img src={imageDefault} alt="Imagem" />
+                  <div className="info-link">
+                    <span className="link-label">{link.label}</span>
+                    <a className="link-url" target="blank" href={link.url}>
+                      {link.url}
+                    </a>
+                  </div>
+                </div>
 
-        {/* <div className="link-content">
-          <span className="link-label">Label</span>
-          <span className="link-url">Url</span>
-        </div>
-
-        <Link className="back-link" to="/links/:id">
-          <FiEdit size={16} color="#fc3847" />
-          Editar
-        </Link>
-        <Link className="back-link" to="/links/:id">
-          <FiTrash2 size={16} color="#fc3847" />
-          Deletar
-        </Link> */}
-      </div>
+                <section>
+                  <Link className="edit-link" to="/links/editar/:id">
+                    <FiEdit size={16} color="#fc3847" />
+                    Editar
+                  </Link>
+                  <button className="delete-link">
+                    <FiTrash2 size={16} color="#fc3847" />
+                    Deletar
+                  </button>
+                </section>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 };
 
-export default ListLinks;
+const mapStateToProps = (state) => {
+  return {
+    links: state.link.links,
+  };
+};
+
+export default connect(mapStateToProps, { linkList })(ListLinks);
